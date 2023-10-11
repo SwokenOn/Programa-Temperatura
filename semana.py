7542f294ccffa80baaebe8b4af85655fb504675f
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-10-11 17:11:22 -0600

    Update semana.py

--- semana.py
+++ semana.py
@@ -5,5 +5,8 @@ class semana:
     id = 0
     dia = date(2023,10,11)
     temperatura = 0.0
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self,id,dia,temperatura) -> None:
+        self.id = id
+        self.dia = dia
+        self.temperatura = temperatura
