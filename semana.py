83d4942267eac952722145fe6f2f46f0ffaa0c6f
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-10-11 17:13:51 -0600

    Update semana.py

--- semana.py
+++ semana.py
@@ -2,11 +2,13 @@ from datetime import date
 
 class semana:
 
-    id = 0
-    dia = date(2023,10,11)
+    id = date(2023,10,11)
+    dia = ""
     temperatura = 0.0
 
     def __init__(self,id,dia,temperatura) -> None:
         self.id = id
         self.dia = dia
         self.temperatura = temperatura
+    
+    def Calcular_Promedio(te):
