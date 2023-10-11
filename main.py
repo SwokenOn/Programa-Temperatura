51182de08ba5b5cd3e7ed0dc7b1d424fedf0595e
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-10-11 17:47:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ for index, dia in enumerate(semana.temperaturas):
     entrada = ctk.CTkEntry(ventana)
     entrada.grid(row=index, column=1, padx=10, pady=5)
     entradas[dia] = entrada
-
+    
 btn_guardar = ctk.CTkButton(ventana, text="Guardar", command=guardar_temperaturas)
 btn_guardar.grid(row=len(semana.temperaturas), column=0, columnspan=2, pady=10)
 ventana.mainloop()
