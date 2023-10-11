ae2bab5c6b3d503411304a200851d2f90a5070eb
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-10-11 17:15:48 -0600

    Update semana.py

--- semana.py
+++ semana.py
@@ -1,14 +1,31 @@
 from datetime import date
 
-class semana:
+class Semana:
+    id = date(2023, 10, 11)
+    temperaturas = {
+        "Lunes": None,
+        "Martes": None,
+        "Miércoles": None,
+        "Jueves": None,
+        "Viernes": None,
+        "Sábado": None,
+        "Domingo": None
+    }
 
-    id = date(2023,10,11)
-    dia = ""
-    temperatura = 0.0
-
-    def __init__(self,id,dia,temperatura) -> None:
+    def __init__(self, id):
         self.id = id
-        self.dia = dia
-        self.temperatura = temperatura
-    
-    def Calcular_Promedio(te):
+
+    def registrar_temperatura(self, dia, temperatura):
+        if dia in self.temperaturas:
+            self.temperaturas[dia] = temperatura
+        else:
+            print("Día no válido.")
+
+    def calcular_promedio(self):
+        total = 0
+        dias_contados = 0
+        for temp in self.temperaturas.values():
+            if temp is not None:
+                total += temp
+                dias_contados += 1
+        return total / dias_contados if dias_contados > 0 else 0
