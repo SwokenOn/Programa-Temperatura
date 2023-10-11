fb43a0689a1e897bcf18cbbc01170455460923a9
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-10-11 17:30:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import customtkinter
 from datetime import date
+import tkinter
 
+def guardar_temperaturas():
+    for dia, entrada in entradas.items():
+        try:
+            temperatura = float(entrada.get())
+            semana.registrar_temperatura(dia, temperatura)
+        except ValueError:
+            pass
 
 ventana = customtkinter.CTk()
+
 
 
 ventana.mainloop()
